676b3a4fa79a06f202166e59cfe89289e60a7787
Denis <molodtsovdenis@gmail.com>
2019-07-24 16:21:27 -0400

    sample

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,5 +1,4 @@
 Connect-PnPOnline -Url https://contoso-admin.sharepoint.com
 Apply-PnPProvisioningTemplate -Path ./PnP_TenantDesign.xml
-
 $script = Get-PnPSiteScript | Where-Object {$_.Title -eq "SPFx Install Script"}
 Get-PnPSiteScript -Identity $script.id | Format-List
