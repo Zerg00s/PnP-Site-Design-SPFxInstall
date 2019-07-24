2354b58a900df837cd9d47e222f9c5619a7db228
Denis <molodtsovdenis@gmail.com>
2019-07-24 16:01:28 -0400

    Issue sample 2

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,8 +1,5 @@
-Connect-PnPOnline -Url https://zergs-admin.sharepoint.com
+Connect-PnPOnline -Url https://contoso-admin.sharepoint.com
 Apply-PnPProvisioningTemplate -Path ./PnP_TenantDesign.xml
 
-Connect-PnPOnline -Url https://zergs.sharepoint.com/Sites/TestCommunity
-
-Invoke-PnPSiteDesign -Identity $SiteDesign -WebUrl $Url | Tee-Object  -Variable resultSiteDesign *>$null
-$resultString = Out-String -InputObject $resultSiteDesign
-Write-Host $resultString
+$script = Get-PnPSiteScript | Where-Object {$_.Title -eq "SPFx Install Script"}
+Get-PnPSiteScript -Identity $script.id | Format-List
--- README.md
+++
@@ -1,1 +0,0 @@
-"# PnP-Site-Design-SPFxInstall" 
